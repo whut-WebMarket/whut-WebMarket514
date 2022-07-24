74100bedab98f486557409f95f5ca4e8c789b553
夜游 <86450324+night247@users.noreply.github.com>
2022-07-24 21:08:28 +0800

    new

---
+++ src/main/java/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HelloController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
